c32d91b8b6c4eba8a22940acf76bdb90ee5325cf
Hong-Layeang <layeang.hong@student.cadt.edu.kh>
2026-04-09 21:36:34 +0700

    allow public access for easier to develop

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -70,8 +70,10 @@ variable "admin_cidr" {
   default     = "127.0.0.1/32"
 
   validation {
-    condition     = can(cidrhost(var.admin_cidr, 0)) && var.admin_cidr != "0.0.0.0/0"
-    error_message = "admin_cidr must be a valid CIDR and must not be 0.0.0.0/0. Use your public IP range, for example x.x.x.x/32."
+    # condition     = can(cidrhost(var.admin_cidr, 0)) && var.admin_cidr != "0.0.0.0/0"
+    # error_message = "admin_cidr must be a valid CIDR and must not be 0.0.0.0/0. Use your public IP range, for example x.x.x.x/32."
+    condition     = can(cidrhost(var.admin_cidr, 0))
+    error_message = "admin_cidr must be a valid CIDR block."
   }
 }
 
